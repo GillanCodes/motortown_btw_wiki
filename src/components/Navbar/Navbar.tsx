173f9174b42d13917a725db9bf3c5dcb90b5e716
gillancodes <gillancodes@laposte.net>
2025-02-24 20:50:38 +0100

    feat: base style navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,9 @@ import "./Navbar.scss";
 export const Navbar = (props : {}) => {
   return (
     <nav className="navbar">
-      
+      <div className="navbar__content">
+        <a href="" className="navbar__button">Home</a> 
+      </div>
     </nav>
   )
 }
