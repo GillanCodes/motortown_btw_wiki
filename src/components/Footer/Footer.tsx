3d4964e99136baab5b028ea1f9585f8db3b89728
gillancodes <gillancodes@laposte.net>
2025-02-26 12:27:49 +0100

    feat: added footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p>This is an opensource project | <a href="https://github.com/gillancodes/motortown_btw_wiki/" target="_blank">Github</a></p>
+      </div>
+    </footer>
+  )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from '../pages/Home/Home';
 import { Navbar } from '../components/Navbar/Navbar';
 import { NotFound } from '../pages/Errors/NotFound';
+import { Footer } from '../components/Footer/Footer';
 
 export default function index() {
   return (
@@ -12,6 +13,7 @@ export default function index() {
           <Route index element={<Home />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
