3815bb1a896fe46cd09b7ba26c9019f6c9c27033
gillancodes <gillancodes@laposte.net>
2025-02-26 12:17:24 +0100

    feat: added head and info section to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,17 @@ import "./Home.scss";
 export default function Home() {
   return (
     <div className="container home__container">
-      <h1>Home</h1>
+
+      <div className="home__head">
+        <img src="/images/logo.png" alt="Motor Town: Behind The Wheel" className="home__head__main-logo" />
+        <h1 className="home__head__title">MotorTown Wiki</h1>
+      </div>
+
+      <div className="home__info">
+        <p><a href="https://motor-town.com/" target="_blank" id="official_website">MotorTown: Behind the wheel</a> is an Open World Driving Simulator with Proper Tire Physics</p><br />
+        <p>The game was released on December 21th, 2021.</p> <a href="https://store.steampowered.com/app/1369670/Motor_Town_Behind_The_Wheel/" target="_blank" className="button">Buy it on Steam</a>
+      </div>
+
 
       <div className="cards">
         <div className="card-container">
