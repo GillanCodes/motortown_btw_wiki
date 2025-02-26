0a412d3dda44081c6aeb7885044387a65077be7f
gillancodes <gillancodes@laposte.net>
2025-02-26 11:41:11 +0100

    feat: added 404 page

---
+++ src/pages/Errors/NotFound.tsx
@@ -0,0 +1,10 @@
+import "./NotFound.scss";
+
+export const NotFound = () => {
+  return (
+    <div className="notfound__container">
+      <h1>404 Error</h1> 
+      <p>The URL you try to reach do not exist :(</p>
+    </div>
+  )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from '../pages/Home/Home';
 import { Navbar } from '../components/Navbar/Navbar';
+import { NotFound } from '../pages/Errors/NotFound';
 
 export default function index() {
   return (
@@ -9,6 +10,7 @@ export default function index() {
         <Navbar />
         <Routes>
           <Route index element={<Home />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
