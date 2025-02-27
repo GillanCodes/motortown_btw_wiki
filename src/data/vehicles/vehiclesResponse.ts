a18b799cf122265ad8c3ec3b777ffc4033b32f90
gillancodes <gillancodes@laposte.net>
2025-02-27 13:32:33 +0100

    feat: chnage data structure

--- src/data/vehicles/vehiclesResponse.ts
+++ src/data/vehicles/vehiclesResponse.ts
@@ -1,12 +1,18 @@
 import Vehicle from "../../models/Vehicle";
 
 import cora from "./cora.json";
+import mitage from "./mitage.json";
+import jemusiSemi from "./jemusi-semi.json";
+import jemusiTank from "./jemusi-tanker.json";
 
 export function getAllVehicles(): Vehicle[]
 {
   var response: Vehicle[] = [];
 
   response.push(new Vehicle(cora));
+  response.push(new Vehicle(mitage));
+  response.push(new Vehicle(jemusiSemi));
+  response.push(new Vehicle(jemusiTank));
 
 
   return response;
