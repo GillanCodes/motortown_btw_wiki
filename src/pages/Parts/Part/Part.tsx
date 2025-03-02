9180638a5b52d1981c32b46d9cd78e50503e4d27
gillancodes <gillancodes@laposte.net>
2025-03-02 21:12:11 +0100

    feat: added single part page

--- src/pages/Parts/Part/Part.tsx
+++ src/pages/Parts/Part/Part.tsx
@@ -1,7 +1,42 @@
+import { useParams } from "react-router";
+import { getAllParts } from "../../../data/parts/partsResponse";
+import PartClass from "../../../models/Part.ts";
+
 export default function Part() {
+  const { slug } = useParams<{ slug: string }>();
+  const parts = getAllParts();
+
+  const partData:PartClass | undefined = parts.find((v: any) => v.slug === slug);
+
+  if (!partData) {
+    return (
+      <div>
+        <p>No part found ...</p>
+      </div>
+    )
+  }
+
+  const part:PartClass = new PartClass(partData);
+
   return (
     <div className="container part_container">
-      Part
+      <div className="grid">
+
+        <div id="title">
+          {part.name}
+        </div>
+        <div id="info">
+          <div className="field">
+            <p className="field__title">Description</p>
+            <p className="field__content">{part.description}</p>
+          </div>
+        </div>
+        <div id="vehicles">
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
