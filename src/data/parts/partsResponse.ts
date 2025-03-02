bd25e0dd78333227213a5b98fc7112a7526fd1ce
gillancodes <gillancodes@laposte.net>
2025-03-02 16:43:14 +0100

    feat :added vehicle and parts for dev purpose

--- src/data/parts/partsResponse.ts
+++ src/data/parts/partsResponse.ts
@@ -2,6 +2,10 @@ import Part from "../../models/Part";
 
 import e1 from './engine_v8_440.json';
 import e2 from './engine_v8_240.json';
+import b1 from './brake_pad1.json';
+import b2 from './brake_power1.json';
+import trubo from './turbo.json';
+import rad from './radiator.json';
 
 export function getAllParts(): Part[]
 {
@@ -9,6 +13,10 @@ export function getAllParts(): Part[]
   
   response.push(new Part(e1));
   response.push(new Part(e2));
+  response.push(new Part(b1));
+  response.push(new Part(b2));
+  response.push(new Part(trubo));
+  response.push(new Part(rad));
   
   return response;
 }
--- src/data/vehicles/vehiclesResponse.ts
+++ src/data/vehicles/vehiclesResponse.ts
@@ -4,6 +4,8 @@ import cora from "./cora.json";
 import mitage from "./mitage.json";
 import jemusiSemi from "./jemusi-semi.json";
 import jemusiTank from "./jemusi-tanker.json";
+import dory from "./dory.json";
+import hana from "./hana.json";
 
 export function getAllVehicles(): Vehicle[]
 {
@@ -13,6 +15,8 @@ export function getAllVehicles(): Vehicle[]
   response.push(new Vehicle(mitage));
   response.push(new Vehicle(jemusiSemi));
   response.push(new Vehicle(jemusiTank));
+  response.push(new Vehicle(dory));
+  response.push(new Vehicle(hana));
 
 
   return response;
