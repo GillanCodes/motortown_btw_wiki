873c0da4cf7ebeb1bb5a684e8daf88a4bf36bd14
gillancodes <gillancodes@laposte.net>
2025-03-06 13:54:05 +0100

    feat: added get and create vehicle

--- server/src/features/Vehicle/vehicle.controller.ts
+++ server/src/features/Vehicle/vehicle.controller.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import vehicleModel from "./vehicle.model";
 import { Info } from "../../../../shared/models/Vehicle";
+import partModel from "../Part/part.model";
+import { isValidObjectId } from "mongoose";
 
 export const getAllVehicles = async (req: Request, res: Response): Promise<any> => {
   const parts = await vehicleModel.find();
   return res.status(201).json(parts);
+}
+
+export const getVehicle = async (req: Request, res: Response): Promise<any> => {
+
+  const { id } = req.params;
+  if (!isValidObjectId(id)) return res.json({ error: "invalid_id" });
+
+  const vehicle = await vehicleModel.findById(id).populate({path: "parts"});
+  if (!vehicle) return res.json({error: "vehicle_not_found"});
+  return res.json(vehicle);
+
 }
 
 export const createVehicle = (req: Request, res: Response): any => {
@@ -25,10 +38,10 @@ export const createVehicle = (req: Request, res: Response): any => {
 
   const filename = req.file.filename;
   const formatedSlug = slug ? slug.toLocaleLowerCase().split(' ').join('_') : name.toLocaleLowerCase().split(' ').join('_');
-  
+
   vehicleModel.create({
     name,
-    slug:formatedSlug,
+    slug: formatedSlug,
     info,
     parts,
     location,
@@ -36,5 +49,31 @@ export const createVehicle = (req: Request, res: Response): any => {
   }).then((data) => {
     return res.json(data);
   }).catch(err => console.log(err))
+}
 
+export const addPartToVehicle = async (req: Request, res: Response): Promise<any> => {
+
+  const { vehicleId, partId } = req.params;
+
+  if (!isValidObjectId(partId) || !isValidObjectId(vehicleId)) return res.json({ error: "not_valid_ids" })
+
+  const vehicle = await vehicleModel.findById(vehicleId);
+  if (!vehicle) return res.json({ error: "vehicle_not_found" });
+
+  const part = await partModel.findById(partId);
+  if (!part) return res.json({ error: "part_not_found" });
+
+  const partToAdd = String(part._id);
+
+  if (!part.vehicles.includes(String(vehicle._id))) {
+    part.vehicles.push(String(vehicle._id));
+    await part.save();
+  }
+
+  if (!vehicle.parts.includes(partToAdd)) {
+    vehicle.parts.push(partToAdd);
+    await vehicle.save();
+  }
+
+  return res.json(vehicle);
 }
--- server/src/features/Vehicle/vehicle.routes.ts
+++ server/src/features/Vehicle/vehicle.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { createVehicle, getAllVehicles } from "./vehicle.controller";
+import { addPartToVehicle, createVehicle, getAllVehicles, getVehicle } from "./vehicle.controller";
 let multerMiddleware = require("../../middlewares/multer.middleware");
 const router:Router = Router();
 
 router.get('/', getAllVehicles);
+router.get('/:id', getVehicle);
 
 router.post('/', multerMiddleware, createVehicle);
 
+router.patch('/:vehicleId/addpart/:partId', addPartToVehicle)
 
 export default router;
 
