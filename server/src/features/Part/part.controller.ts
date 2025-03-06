6dbd2ace9144858b3bfef6e0e26be4c3e61d30a9
gillancodes <gillancodes@laposte.net>
2025-03-06 11:00:43 +0100

    feat: added editPart

--- server/src/features/Part/part.controller.ts
+++ server/src/features/Part/part.controller.ts
@@ -1,24 +1,25 @@
-import { Request, Response } from "express"; 
+import { Request, Response } from "express";
 import partModel from "./part.model";
+import { isValidObjectId } from "mongoose";
 
 type BettermentType = {
-  size:number,
-  unit:string
+  size: number,
+  unit: string
 }
 
 type CategoryType = {
-  main:string,
-  sub:string
+  main: string,
+  sub: string
 }
 
-export const getAllParts = async (req:Request, res:Response):Promise<any> => {
+export const getAllParts = async (req: Request, res: Response): Promise<any> => {
   const parts = await partModel.find();
   return res.status(201).json(parts);
 }
 
 export const createPart = (req: Request, res: Response): any => {
 
-  const { name, slug, category, betterment, price, mass, description }: { name: string, slug?: string, category:CategoryType, betterment?: BettermentType, price?:number, mass?:number, description?:string } = req.body;
+  const { name, slug, category, betterment, price, mass, description }: { name: string, slug?: string, category: CategoryType, betterment?: BettermentType, price?: number, mass?: number, description?: string } = req.body;
 
   if (!name)
     return res.json({ error: "empty_name" }); // TODO Handle Error
@@ -44,4 +45,33 @@ export const createPart = (req: Request, res: Response): any => {
     console.log(err);
     return res.send(err)
   })
+}
+
+export const editPart = async (req: Request, res: Response): Promise<any> => {
+
+  const { id } = req.params;
+  if (!isValidObjectId(id)) return res.json({error: "invalid_id"});
+
+  const { name, slug, category, betterment, price, mass, description }: { name: string, slug?: string, category: CategoryType, betterment?: BettermentType, price?: number, mass?: number, description?: string } = req.body;
+
+  if (!name) return res.json({ error: "empty_name" }); // TODO Handle Error
+  if (!category) return res.json({ error: "empty_category" }); // TODO Handle Error
+  
+  let formatedSlug = slug ? slug.toLocaleLowerCase().split(' ').join('_') : name.toLocaleLowerCase().split(' ').join('_');
+
+  partModel.findByIdAndUpdate(id, {
+    $set: {
+      name,
+      slug:formatedSlug,
+      category,
+      description,
+      info: {
+        betterment,
+        price,
+        mass
+      }
+    }
+  }, {new:true, upsert:true}).then((data) => {
+    res.status(201).send(data);
+  }).catch(err => console.log(err))
 }
--- server/src/features/Part/part.routes.ts
+++ server/src/features/Part/part.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createPart, getAllParts } from "./part.controller";
+import { createPart, editPart, getAllParts } from "./part.controller";
 const router:Router = Router();
 
 router.get('/', getAllParts);
 
 router.post('/', createPart);
+
+router.put('/:id', editPart);
 
 export default router;
 
